7c606ce2fb69bbcd467e2edca2399916d163284d
Pradeep Thiruppathi <125948189+pradeepthiruppathi@users.noreply.github.com>
2024-12-08 21:14:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/Layout'; // Import the Layout component
 import Dashboard from './components/Dashboard';
 import Users from './components/Users';
